cc943e262c77e59565ce101b078263973fe00e20
Ben <statonbe@gmail.com>
2024-08-01 10:08:59 -0700

    Add all code for GLM analysis #2a
    2-gen RRS without F1 covariate

---
+++ 02a-2gen-RRS/02a-03-boot.R
@@ -0,0 +1,21 @@
+
+## SETUP WORKSPACE FOR THIS ANALYSIS ##
+# -> sets output directory           ##
+# -> loads data                      ##
+# -> loads any existing output files ##
+source("02a-2gen-RRS/02a-00-setup.R")
+
+# set bootstrap dimensions; use the args object if it exists
+if (exists("the_args")) {
+  nboot = the_args$nboot 
+  ncpu = the_args$ncpu 
+} else {
+  nboot = 500
+  ncpu = max(min(parallel::detectCores() - 2, 5), 1)
+}
+
+# run the bootstrap
+boot_preds = bootstrap(best_model, dat, nboot = nboot, ncpu = ncpu)
+
+# save the output
+saveRDS(boot_preds, file = file.path(out_dir, "boot_preds.rds"))
